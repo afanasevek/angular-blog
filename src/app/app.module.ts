04f81194718889f8717e860e83d053c08b1c32f3
Afanasev Evgeny <afanasevek@gmail.com>
2021-01-05 09:10:27 +0300

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar'; 
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon'; 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from "@angular/common/http";
+import {MatInputModule} from '@angular/material/input'; 
+import {MatRippleModule} from '@angular/material/core'; 
+import { AutocompleteLibModule } from 'angular-ng-autocomplete';
+import { FormsModule } from '@angular/forms';
+
+
+import {MatAutocompleteModule} from '@angular/material/autocomplete'; 
+import 'hammerjs';
 
 @NgModule({
   declarations: [
@@ -10,7 +23,18 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    FlexLayoutModule,
+    MatButtonModule,
+    MatIconModule,
+    HttpClientModule,
+    MatInputModule,
+    MatRippleModule,
+    AutocompleteLibModule,
+    FormsModule,
+    MatAutocompleteModule
   ],
   providers: [],
   bootstrap: [AppComponent]
